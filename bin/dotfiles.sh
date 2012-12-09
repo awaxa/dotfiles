08d2f6627ebafd6b58bd4ec44e6485a52bec6fbd
Greg Kitson <greg.kitson@gmail.com>
2012-12-09 16:26:33 -0500

    now THIS is a dotfiles script I can be proud of

--- bin/dotfiles.sh
+++ bin/dotfiles.sh
@@ -1,43 +1,60 @@
 #!/bin/bash
 
-installhome="$HOME/dotfiles/home"
-autoinstall="$HOME/dotfiles/bin"
+clonepath=$HOME
+dotfiles=$clonepath/dotfiles
+backup=$dotfiles/backup
+
+homeinstall="$dotfiles/home"
+autoinstall="$dotfiles/bin"
 
 first=0
 
-if [ ! -d $HOME/dotfiles/.git ]
+if [ ! -d $dotfiles/.git ]
 then
 	curl -s https://raw.github.com/awaxa/dotfiles/master/README.md
 	echo ; echo
 	echo "This script will proceed to clone https://github.com/awaxa/dotfiles.git into your home directory and install symlinks to its contents in the appropriate places."o
-	echo "By default this will move your existing dotfiles to \$filename.backup-$(date +%Y.%m.%d-%H%M%S) and install symlinks in their place?"
+	echo "By default this will move your existing dotfiles to $backup/backup.\$filename.$(date +%Y.%m.%d-%H%M%S) and install symlinks in their place?"
 	read -p "Press enter to continue installing dotfiles in $HOME  " gogogo
 	echo "Beginning..."
 	echo
-	cd $HOME
+	cd $clonepath
 	git clone https://github.com/awaxa/dotfiles.git
 else
-	cd $HOME/dotfiles
+	cd $dotfiles
 	git checkout master
 	git pull
 fi
-cd $HOME
+cd $clonepath
 
-filelist=$(find $installhome $autoinstall -type f | sed "s#^$HOME/dotfiles/home/##g")
-dirlist=$autoinstall" "$(find $installhome $autoinstall -type d -depth +0 | sed "s#^$HOME/dotfiles/home/##g")
+filelist=$(find $homeinstall -type f | sed "s#^$homeinstall/##g")" "$(find $autoinstall -type f | sed "s#^$dotfiles/##g")
+dirlist=$(find $homeinstall -type d -depth +0 | sed "s#^$homeinstall/##g")" "$(find $autoinstall -type d | sed "s#^$dotfiles/##g")
 
-for f in $filelist
-do
-	[ -f $HOME/$f ] && mv -v $HOME/$f $HOME/$f.backup-$(date +%Y.%m.%d-%H%M%S)
-done
+mkdir -pv $backup
 
 for d in $dirlist
 do
-	mkdir -pv $HOME/$d
+	mkdir -pv $clonepath/$d
 done
 
 for f in $filelist
 do
-	[ ! -h $HOME/$f ] && ln -sv $HOME/dotfiles/$f $HOME/$f
+	if [ -h $clonepath/$f ] && [ "$(readlink $clonepath/$f)" == "$homeinstall/$f" ] 
+	then
+		break
+	elif [ -a $clonepath/$f ]
+	then
+		mv -v $clonepath/$f $backup/backup.$f.$(date +%Y.%m.%d-%H%M%S)
+	fi
+
+	if [ -f $homeinstall/$f ]
+	then
+		ln -sv $homeinstall/$f $clonepath/$f
+	elif [ -f $dotfiles/$f ]
+	then
+		ln -sv $dotfiles/$f $clonepath/$f
+	else
+		echo "exception: $f doesn't exist in $dotfiles nor $homeinstall"
+	fi
 done
 
