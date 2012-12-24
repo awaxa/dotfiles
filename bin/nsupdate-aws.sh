d9b9d2c365d093c83bd39672e14295e9eeb63a06
Greg Kitson <greg.kitson@gmail.com>
2012-12-23 19:36:22 -0500

    disable dd-wrt scrape for new tomato router

--- bin/nsupdate-aws.sh
+++ bin/nsupdate-aws.sh
@@ -44,8 +44,9 @@ fi
 
 IPREGEX='[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}'
 
-ip=$(curl -s "http://$ddwrt/" | grep WAN | egrep -o $IPREGEX) #scrape IP from dd-wrt status page
+#ip=$(curl -s "http://$ddwrt/" | grep WAN | egrep -o $IPREGEX) #scrape IP from dd-wrt status page
 #checks=( "http://checkip.dyndns.org/" "http://icanhazip.com" "http://ifconfig.me" )
+ip=$(curl -s "http://checkip.dyndns.org/" | egrep -o $IPREGEX)
 
 dig=$(dig +short +trace $record.$zone. | grep ^A | cut -f2 -d' ')
 
