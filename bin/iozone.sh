0b51af97fa4b9d8db223a92a8b3ac5fcd5de292e
Greg Kitson <greg.kitson@gmail.com>
2012-12-29 01:16:40 -0500

    right?

--- bin/iozone.sh
+++ bin/iozone.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-TESTS=3
-
 NAME="$1"
+MOUNT="$2"
+
+TESTS=3
 
 for i in {1..$TESTS} ; do
         TIMESTAMP=$(date +%Y.%m.%d.%H%M)
-        iozone -R -a -z -b $NAME.$TIMESTAMP.xls -g 8g -f $2/file.test 2>&1 | tee /root/$NAME.$TIMESTAMP.log
+        iozone -R -a -z -b $NAME.$TIMESTAMP.xls -g 8g -f $MOUNT/file.test 2>&1 | tee /root/$NAME.$TIMESTAMP.log
+        rm -f $MOUNT/file.test
 done
 
