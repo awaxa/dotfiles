d183ca634937959d9b7d4aa2a89127dbbd82030c
Greg Kitson <greg.kitson@gmail.com>
2012-12-29 22:11:03 -0500

    better

--- bin/iozone.sh
+++ bin/iozone.sh
@@ -5,9 +5,11 @@ MOUNT="$2"
 
 TESTS=1
 
+mkdir -p $NAME
+
 for i in {1..$TESTS} ; do
         TIMESTAMP=$(date +%Y.%m.%d.%H%M)
-        iozone -R -a -z -b $NAME.$TIMESTAMP.xls -g 2g -f $MOUNT/file.test 2>&1 | tee /root/$NAME.$TIMESTAMP.log
+        iozone -R -a -z -b $NAME.$TIMESTAMP.xls -g 2g -f $MOUNT/file.test 2>&1 | tee $NAME/$NAME.$TIMESTAMP.log
         rm -f $MOUNT/file.test
 done
 
