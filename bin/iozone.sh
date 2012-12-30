601bf59f7ee39db9ee212fa5e62c7b69571e1ae5
Greg Kitson <greg.kitson@gmail.com>
2012-12-29 21:56:41 -0500

    iops aren't free and that was really taking too long

--- bin/iozone.sh
+++ bin/iozone.sh
@@ -3,11 +3,11 @@
 NAME="$1"
 MOUNT="$2"
 
-TESTS=3
+TESTS=1
 
 for i in {1..$TESTS} ; do
         TIMESTAMP=$(date +%Y.%m.%d.%H%M)
-        iozone -R -a -z -b $NAME.$TIMESTAMP.xls -g 8g -f $MOUNT/file.test 2>&1 | tee /root/$NAME.$TIMESTAMP.log
+        iozone -R -a -z -b $NAME.$TIMESTAMP.xls -g 2g -f $MOUNT/file.test 2>&1 | tee /root/$NAME.$TIMESTAMP.log
         rm -f $MOUNT/file.test
 done
 
