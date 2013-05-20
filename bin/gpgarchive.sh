9160cee95965da3d9fb31c863933415acbdc0fc2
Greg Kitson <greg.kitson@gmail.com>
2013-05-20 19:12:53 -0400

    woo, comments

--- bin/gpgarchive.sh
+++ bin/gpgarchive.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
+# i wouldn't think the ifs setting would live past the script,
+# but this SAVEIFS stuff was in the template I used
 SAVEIFS=$IFS
+
+# set IFS to support bash loops given filenames with spaces
 IFS=$(echo -en "\n\b")
+
 for i in $* ; do 
   cp -a "$i" "$i-goats"
   cd "$i-goats"
   for d in $(find . -type d)
   do
+    # rmdir fails unless the dir is empty
     rmdir "$d"
   done
   tar czvf ../$i.tar.gz *
