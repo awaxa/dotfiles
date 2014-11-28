90b23755c591151a308cd5568ca4612c9542ed19
Greg Kitson <greg.kitson@gmail.com>
2014-11-28 15:21:24 -0800

    print may or may not be cleaner than puts

--- home/bin/battinfo.rb
+++ home/bin/battinfo.rb
@@ -1,24 +1,20 @@
 #!/usr/bin/env ruby
 
-out = ''
-
 pmset = `pmset -g ps`
 
 if pmset =~ /'(.*)'/
   case $~[1]
   when 'AC Power'
-    out += "\u{26A1}  "
+    print "\u{26A1}  "
   when 'Battery Power'
-    out += "\u{1F50B}  "
+    print "\u{1F50B}  "
   end
 end
 
-out += "#{$~[1]} " if pmset =~ /(\d{1,3}%);/
+print "#{$~[1]} " if pmset =~ /(\d{1,3}%);/
 
 if pmset =~ /(\d+:\d+)/
-  out += $~[1] unless $~[1] == '0:00'
+  print $~[1] unless $~[1] == '0:00'
 else
-  out += "\u{1F551} "
+  print "\u{1F551} "
 end
-
-puts out
